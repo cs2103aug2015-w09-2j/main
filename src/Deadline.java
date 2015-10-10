ea6e85bc2c8748d30d244fa49264dfe02ca318f4
Ravi Chandak <ravi.chandak901@gmail.com>
2015-10-10 15:59:05 +0800

    included javadoc comments

--- src/Deadline.java
+++ src/Deadline.java
@@ -4,18 +4,37 @@ public class Deadline extends Task{
 	private DateClass endDate;
 	private TimeClass endTime;
 	
+	/**
+	 * Description Constructor : Creates a deadline task, which is a subclass of the Task abstract class. 
+	 * @param description
+	 * @param endDate
+	 * @param endTime
+	 */
 	public Deadline(String description, DateClass endDate, TimeClass endTime){
 		super(description);
 		this.endDate = endDate;
 		this.endTime = endTime;
 	}
+	/**
+	 * Description Gets the endDate of the deadline
+	 * @return DateClass
+	 */
 	public DateClass getEndDate(){
 		return endDate;
 	}
 	
+	/**
+	 * Description Gets the endTime of the deadline
+	 * @return TimeClass
+	 */
 	public TimeClass getEndTime(){
 		return endTime;
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
 	public String toString(){
 		String output = "";
 		output = this.getDescription() + " " + this.getEndDate().toString() + " " + this.getEndTime().toString(); 
