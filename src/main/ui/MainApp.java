ca66725baed9f20f5e5bf7755a620a2111129284
Teddy Hartanto <hartantoteddy@yahoo.com>
2015-10-13 13:37:54 +0800

    Imported the GUI files from my local work

--- src/main/ui/MainApp.java
+++ src/main/ui/MainApp.java
@@ -1,5 +1,90 @@
 package main.ui;
 
-public class MainApp {
+import java.io.IOException;
 
+import javafx.application.Application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+
+import javafx.scene.layout.GridPane;
+
+import javafx.stage.Stage;
+
+import main.ui.view.MainLayoutController;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private GridPane mainLayout;
+	private ObservableList<String> tasks;
+	// private Logic logic;
+
+	public MainApp() {
+		tasks = FXCollections.observableArrayList();
+
+	}
+
+	/*
+	 * Getters
+	 */
+	public ObservableList<String> getTasks() {
+		return tasks;
+	}
+
+	/**
+	 * Initializes the scene and display the stage
+	 */
+	public void showMainLayout() {
+		try {
+			// Load the scene graph from MainLayout.fxml
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/MainLayout.fxml"));
+			mainLayout = (GridPane) loader.load();
+
+			// Show the scene containing the main layout
+			Scene scene = new Scene(mainLayout, 900, 600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			// Set up the controller
+			MainLayoutController mainLayoutController = loader.getController();
+			mainLayoutController.setMainApp(this);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void init() {
+		/*
+		 * This part should call
+		 */
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Vodo");
+
+		// MainApp should check for a config file (need to decide on name) << create a new Configuration class, tell logic to use?
+		// If config file exists
+		//	Get the specified storage location, setup Logic with storage location
+		// Else
+		//  Display a new dialog asking for location to store data
+		//	setup Logic with the storage location
+
+		// pass observable list of tasks to logic
+		showMainLayout();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/main/ui/view/MainLayoutController.java
+++ src/main/ui/view/MainLayoutController.java
@@ -1,5 +1,48 @@
 package main.ui.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import main.ui.MainApp;
+
 public class MainLayoutController {
 
+	/**
+	 * Fields to point to UI components
+	 */
+	@FXML
+	private ListView<String> eventsListView;
+	@FXML
+	private ListView<String> deadlinesListView;
+	@FXML
+	private ListView<String> floatingTasksListView;
+	@FXML
+	private TextField commandBox;
+
+	// Reference to Main App
+	private MainApp mainApp;
+
+	public MainLayoutController() {
+	}
+
+	public void initialize() {
+		/*
+		 * ListView cell factory can be used to customize the content
+		 */
+	}
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+
+		// Add Observable List data to eventsListView
+		eventsListView.setItems(mainApp.getTasks());
+	}
+
+	@FXML
+	public void getCommand() {
+		String entry = commandBox.getText(); // rename to entry
+		// mainApp.processCommand(command); // pass command to logic
+		eventsListView.getItems().add(entry);
+		commandBox.setText("");
+	}
 }
