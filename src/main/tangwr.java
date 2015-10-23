325fb630296c0e04d2c98f971e4ec3606d248f0e
tangwr <firebolt2000@hotmail.com>
2015-10-24 01:01:55 +0800

    search, read, write done
    Bug for read, write fixed

--- src/main/tangwr.java
+++ src/main/tangwr.java
@@ -4,27 +4,61 @@ import java.text.ParseException;
 import java.util.ArrayList;
 
 public class tangwr {
+	
 	public static void main(String[] args) throws NoSuchFieldException, ParseException
 	{
-		JsonFile js = new JsonFile("data.json");
 		FileStorage fs = new FileStorage();
-		DateClass date1 = new DateClass(11, 11);
-		DateClass date2 = new DateClass(12, 11);
-		TimeClass time1 = new TimeClass("1121");
-		TimeClass time2 = new TimeClass("2212");
+		//JsonFile js = new JsonFile("data.json");
 		
-		Event e = new Event("pig", date1, time1, date2, time2);
-		Floating f = new Floating("hello");
-		Deadline d = new Deadline("dog", date1, time2);
+		DateClass date1 = new DateClass(3, 1);
+		DateClass date2 = new DateClass(4, 1);
+		TimeClass time1 = new TimeClass("1500");
+		TimeClass time2 = new TimeClass("1700");
+		
+		Event e = new Event("jad", date1, time1, date2, time2);
+		Floating f = new Floating("jade");
+		Deadline d = new Deadline("eejade", date1, time1);
 		
 		
+		fs.writeTask(d);
+		fs.writeTask(f);
+		fs.writeTask(e);
+		
+		ArrayList<ArrayList<Task>> tk = fs.search("4/1");
+		
+		ArrayList<Task> t = tk.get(0);
+		
+		System.out.println("Event");
+		for(int i = 0; i<t.size(); i++){
+			Task k = t.get(i);
+			System.out.println(k.getDescription() + " " + ((Event)k).getStartDate().toString() + " " + ((Event)k).getStartTime().toString()
+					+ " " + ((Event)k).getEndDate().toString() + " " + ((Event)k).getEndTime().toString());
+		}
+		
+		
+		ArrayList<Task> dead = tk.get(1);
+		System.out.println("Deadline");
+		for(int i = 0; i<dead.size(); i++){
+			Task k = dead.get(i);
+			System.out.println(k.getDescription() + " " + ((Deadline)k).getEndDate().toString() + " " + ((Deadline)k).getEndTime().toString());
+		}
+		
+		System.out.println("Floating");
+		ArrayList<Task> ff = tk.get(2);
+		for(int i = 0; i<ff.size(); i++){
+			Task k = ff.get(i);
+			System.out.println(k.getDescription());
+		}
+		
+		System.out.println("Hello");
+		//js.search("he");
 		//js.jsonWriteTask(event);
 		//js.jsonWriteTask(f);
 		//fs.writeTask(e);
 		//js.jsonWriteTask(d);
 		
 		//js.getEventTask();
-		
+		/*
 		ArrayList<Task> t = fs.readEventTask();
 		
 		for(int i = 0; i<t.size(); i++){
@@ -45,6 +79,10 @@ public class tangwr {
 			Task k = ff.get(i);
 			System.out.println(k.getDescription());
 		}
-		System.out.println("Hello");
+		
+		*/
+		
+		
 	}
+
 }
